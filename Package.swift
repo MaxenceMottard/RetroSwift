ac388fc1c55ccc6ea7a229d601ee34fd381b002c
Maxence Mottard <mottard.maxence@gmail.com>
2022-11-17 19:42:41 +0100

    implement swiftlint and swiftformat

--- Package.swift
+++ Package.swift
@@ -7,26 +7,32 @@ let package = Package(
     name: "RetroSwift",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RetroSwift",
-            targets: ["RetroSwift"]),
+            targets: ["RetroSwift"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/MaxenceMottard/SPMUtilitiesPlugins", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RetroSwift",
-            dependencies: []),
+            dependencies: [],
+            plugins: [
+                .plugin(name: "SwiftLintPlugin", package: "SPMUtilitiesPlugins"),
+            ]
+        ),
         .testTarget(
             name: "RetroSwiftTests",
-            dependencies: ["RetroSwift"]),
+            dependencies: ["RetroSwift"]
+        ),
     ]
 )
